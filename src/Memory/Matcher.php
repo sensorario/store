424bfc8159dfee52f2d312d191fdeb2b3232886b
Simone Gentili <sensorario@gmail.com>
2019-04-28 06:30:46 +0200

    move match logic in a separate service

---
+++ src/Memory/Matcher.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Memory;
+
+class Matcher
+{
+    private $haystack;
+
+    public function setData($haystack)
+    {
+        $this->haystack = $haystack;
+    }
+
+    public function knows($needleName, $needleValue)
+    {
+        if (current($this->haystack) == $needleValue) {
+            return true;
+        }
+
+        foreach ($this->haystack as $fieldName => $value) {
+            if ($needleName == $fieldName && $needleValue == $value) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
